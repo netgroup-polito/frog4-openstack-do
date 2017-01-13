2876c04809097ba72c791cc3b29d20c66b8272fe
Roberto Bonafiglia <roberto.bonafiglia@gmail.com>
2017-01-13 11:48:33 +0100

    Update submodule and fix bug

--- do_core/nffg_manager.py
+++ do_core/nffg_manager.py
@@ -98,7 +98,7 @@ class NFFG_Manager(object):
             headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
             resp = requests.get(TEMPLATE_REPOSITORY_URL + actual_uri, headers=headers)
             resp.raise_for_status()
-            template_dict = json.loads(json.loads(resp.text))
+            template_dict = resp.json()
             self.stored_templates[uri] = template_dict
             return template_dict
         except Exception as ex:

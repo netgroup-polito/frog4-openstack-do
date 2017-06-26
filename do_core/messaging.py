520fbb8f9d5ea47651fbcef52800f80ad8526ea8
Roberto Bonafiglia <roberto.bonafiglia@gmail.com>
2017-06-26 15:41:53 +0200

    Restore previous deleted commit

--- do_core/messaging.py
+++ do_core/messaging.py
@@ -25,7 +25,7 @@ class Singleton(type):
 class DD_client(ClientSafe):
 
     def __init__(self, name, dealerurl, customer, keyfile, topic, message):
-        super().__init__(name=name, dealerurl=dealerurl, customer=customer, keyfilekeyfile)
+        super().__init__(name=name, dealerurl=dealerurl, customer=customer, keyfile=keyfile)
         self._registered = False
         self.topic = topic
         self.message = message
